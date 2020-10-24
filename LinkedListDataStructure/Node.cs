ca476ea8325f3dadaaf8d12577ce57b89df793b7
Aseem <aseemanand2@gmail.com>
2020-10-24 15:44:20 +0530

    [Aseem] Added code to add data to the end of linked list

--- LinkedListDataStructure/Node.cs
+++ LinkedListDataStructure/Node.cs
@@ -1,9 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Node.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace LinkedListDataStructure
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Created Node class with fields data and next
+    /// </summary>
     public class Node
     {
         public int data;
--- LinkedListDataStructure/Program.cs
+++ LinkedListDataStructure/Program.cs
@@ -1,11 +1,22 @@
-﻿using System;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace LinkedListDataStructure
 {
+    using System;
     class Program
     {
         static void Main(string[] args)
         {
+            LinkedList linkedList = new LinkedList();
+            linkedList.InsertAtLast(56);
+            linkedList.InsertAtLast(30);
+            linkedList.InsertAtLast(70);
+            linkedList.Display();
+            Console.ReadKey();
         }
     }
 }
